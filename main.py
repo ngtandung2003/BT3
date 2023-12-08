e1cea1874f7576b27ad54499df6159176f3a5c7a
ngtandung2003 <141381365+ngtandung2003@users.noreply.github.com>
2023-12-08 08:53:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,11 @@ txt = st.text_area('', '')
 
 if txt != '';
   if st.button('Predict'):
-      feature_vector = encoder.transform([txt])
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
+
+    st.header('Result')
+    st.text(class_list[label])
+    
+
+
