03a5d8cd68a794397b10c8bb821e5d72563e41c7
ngtandung2003 <141381365+ngtandung2003@users.noreply.github.com>
2023-12-08 08:20:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
 
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+
+st.title('Sentiment analysis from Vietnamese students' feedback')
